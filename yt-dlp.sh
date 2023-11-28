e5ac62cf8d9344b5b389c2b76f0dc871f1f562ba
mr-mittens <pegasus146@protonmail.com>
2023-11-28 12:51:25 +0530

    updated : lists

--- yt-dlp.sh
+++ yt-dlp.sh
@@ -2,19 +2,25 @@
 
 read -p "Enter command (ytdl-list/ytdl-audio/ytdl/ytdl-show): " selected_command
 
-if [[ "$selected_command" == "ytdl-list" ]]; then
-    read -p "Enter the YouTube playlist URL: " playlist_url
-    yt-dlp -o "%(uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" "$playlist_url" --
-elif [[ "$selected_command" == "ytdl-audio" ]]; then
-    read -p "Enter the YouTube video URL: " video_url
-    yt-dlp --extract-audio --audio-format mp3 --audio-quality 0 "$video_url"
-elif [[ "$selected_command" == "ytdl" ]]; then
-    read -p "Enter the YouTube video URL: " video_url
-    yt-dlp "$video_url"
-elif [[ "$selected_command" == "ytdl-show" ]]; then
-    read -p "Enter the YouTube video URL: " video_url
-    yt-dlp --list-formats "$video_url"
-else
-    echo "Invalid command. Usage: ytdl-list OR ytdl-audio OR ytdl OR ytdl-show"
-fi
+case "$selected_command" in
+    "ytdl-list")
+        read -p "Enter the YouTube playlist URL: " playlist_url
+        yt-dlp -o "%(uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" "$playlist_url" --
+        ;;
+    "ytdl-audio")
+        read -p "Enter the YouTube video URL: " video_url
+        yt-dlp --extract-audio --audio-format mp3 --audio-quality 0 "$video_url"
+        ;;
+    "ytdl")
+        read -p "Enter the YouTube video URL: " video_url
+        yt-dlp "$video_url"
+        ;;
+    "ytdl-show")
+        read -p "Enter the YouTube video URL: " video_url
+        yt-dlp --list-formats "$video_url"
+        ;;
+    *)
+        echo "Invalid command. Usage: ytdl-list OR ytdl-audio OR ytdl OR ytdl-show"
+        ;;
+esac
 
